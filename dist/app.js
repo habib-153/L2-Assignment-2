f79bcf3ee41606ed3f39dd2ab706caccf6bd9969
habib-153 <h.r.sihab155@gmail.com>
2024-05-22 07:08:51 +0600

    deployed

--- dist/app.js
+++ dist/app.js
@@ -5,14 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const product_routes_1 = require("./app/modules/product.routes");
+const product_routes_1 = require("./app/modules/products/product.routes");
+const order_routes_1 = require("./app/modules/orders/order.routes");
 const app = (0, express_1.default)();
 //const port = 3000
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 //  application routes
-app.use("/api/products", product_routes_1.ProductRoutes);
+app.use('/api/products', product_routes_1.ProductRoutes);
+app.use('/api/orders', order_routes_1.OrderRoutes);
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.send('Hello');
 });
 exports.default = app;
